07fb1f1164467a7ef1d9b9a5d58182ede2f3cd75
JohnPaulOtieno <johnpaul9667@gmail.com>
2023-07-15 17:34:13 +0300

    Add about page

--- Acholaicing/urls.py
+++ Acholaicing/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
     path('', include("homepage.urls")),
+    path('about/', include("about.urls")),
 ]
---
+++ about/views.py
@@ -0,0 +1,4 @@
+from django.views.generic.base import TemplateView
+
+class AboutView(TemplateView):
+    template_name = "about/about.html"
